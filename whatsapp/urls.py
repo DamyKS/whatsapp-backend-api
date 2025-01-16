612ea9dafe258c9709b993a8797b3023a64cf98a
damyks <damianojeh@gmail.com>
2025-01-16 11:28:48 +0100

    added api/v1 prefix to urls

--- whatsapp/urls.py
+++ whatsapp/urls.py
@@ -5,17 +5,14 @@ from dj_rest_auth import urls as dj_rest_auth_urls
 from accounts.views import CustomRegisterView, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chat.urls')),
-    path('', include('accounts.urls')),
-    path('status/', include('status.urls')),
-    # path('rest-auth/login/', CustomLoginView.as_view(), name='rest_login'),
-    # path('rest-auth/logout/', CustomLogoutView.as_view(), name='rest_login'),
-    # path('rest-auth/', include('dj_rest_auth.urls')),
-    path('rest-auth/login/', CustomLoginView.as_view(), name='rest_login'),
-    path('rest-auth/logout/', CustomLogoutView.as_view(), name='rest_logout'),
+    path('api/v1/admin/', admin.site.urls),
+    path('api/v1/', include('chat.urls')),
+    path('api/v1/accounts', include('accounts.urls')),
+    path('api/v1/status/', include('status.urls')),
+    path('api/v1/rest-auth/login/', CustomLoginView.as_view(), name='rest_login'),
+    path('api/v1/rest-auth/logout/', CustomLogoutView.as_view(), name='rest_logout'),
     # Filter out login/logout URLs from dj_rest_auth
     *[url for url in dj_rest_auth_urls.urlpatterns 
       if url.name not in ['rest_login', 'rest_logout']],
-    path('rest-auth/registration/', CustomRegisterView.as_view(), name='rest_register'),
+    path('api/v1/rest-auth/registration/', CustomRegisterView.as_view(), name='rest_register'),
 ]
